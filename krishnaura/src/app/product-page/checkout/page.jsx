462b77ae5bb216f21060504408ebabeeaff00be5
HRDs-VERSE <hrds.verse@gmail.com>
2024-04-11 11:17:07 +0530

    suspense error 10

--- krishnaura/src/app/product-page/checkout/page.jsx
+++ krishnaura/src/app/product-page/checkout/page.jsx
@@ -14,7 +14,7 @@ function Search() {
 }
 
 function Searchbar() {
-    const { id, quantity, size, color } = Search();
+    // const { id, quantity, size, color } = Search();
     return (
         <Suspense>
             <div className='mt-[10rem]'>hello</div>
