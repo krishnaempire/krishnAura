ac930dc427403ee24cbd6d50a123eb800916c441
HRDs-VERSE <hrds.verse@gmail.com>
2024-04-11 11:11:07 +0530

    suspense error check 9.7

--- krishnaura/src/app/product-page/checkout/page.jsx
+++ krishnaura/src/app/product-page/checkout/page.jsx
@@ -1,14 +1,11 @@
 'use client'
-import { useSearchParams, useRouter } from 'next/navigation'
-import { Suspense } from 'react'
-import React, { useEffect, useState } from 'react'
-import { Button }  from '@nextui-org/react'
-import useProductApi from '@/api/useProductApi';
-import { Checkout } from '@/components/Checkout'
-import {Spinner} from "@nextui-org/react"
 
-function GetParams() {
+import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+
+function Search() {
     const searchParams = useSearchParams()
+
     const id = searchParams.get('id');
     const quantity = searchParams.get('quantity');
     const size = searchParams.get('size');
@@ -16,59 +13,11 @@ function GetParams() {
     return { id, quantity, size, color };
 }
 
-function CheckoutPage() {
-    const router = useRouter()
-    const { id, quantity, size, color } = GetParams();
-    const [searchParamsReady, setSearchParamsReady] = useState(false);
-    const [product, setProduct] = useState()
-    const { getProduct } = useProductApi()
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await getProduct(id)
-                setProduct(data)
-            } catch (error) {
-                console.error("Error fetching product data:", error);
-            }
-        };
-        fetchData();
-    }, [])
-
-    useEffect(() => {
-        if (id !== null && quantity !== null && size !== null && color !== null) {
-            setSearchParamsReady(true);
-        }
-    }, [id, quantity, size, color]);
-
-    const handleBack = () => {
-        router.push(`/product-page/${id}`)
-    }
-
-    if (!searchParamsReady || !product) {
-        return (
-            <div className='w-full h-screen flex justify-center items-center'>
-                <Spinner size='lg' />
-            </div>
-        );
-    }
-
+export function Searchbar() {
+    const { id, quantity, size, color } = useSearchParams();
     return (
         <Suspense>
-            <div className='mt-[6rem] flex '>
-                <Button
-                    variant={"bordered"}
-                    className='relative top-4 left-2'
-                    onClick={handleBack}
-                    disabled={!product?._id}
-                >
-                    Back
-                </Button>
-                {product?._id && (
-                    <Checkout product={product} color={color} size={size} quantity={quantity} />
-                )}
-            </div>
+            <div className='mt-[10rem]'>hello</div>
         </Suspense>
     )
 }
-export default CheckoutPage
