07049d0678346a52e5161ac5d9cea9de33fc3b44
HRDs-VERSE <hrds.verse@gmail.com>
2024-04-11 10:23:29 +0530

    suspense error check 9.4

--- krishnaura/src/app/product-page/checkout/page.jsx
+++ krishnaura/src/app/product-page/checkout/page.jsx
@@ -4,12 +4,12 @@ import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
  
 function Search() {
-  const searchParams = useSearchParams()
- 
-  return <input placeholder="Search..." />
+    
+    return <input placeholder="Search..." />
 }
- 
+
 function CheckoutPage() {
+    const searchParams = useSearchParams()
   return (
     // You could have a loading skeleton as the `fallback` too
     <Suspense>
