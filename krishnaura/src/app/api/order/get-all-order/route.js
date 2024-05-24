562edfa7e7851628e1f956ab2807e89ef9638a10
krishnaempire <krishnaempire77@gmail.com>
2024-05-24 13:33:47 +0000

    test1.1

--- krishnaura/src/app/api/order/get-all-order/route.js
+++ krishnaura/src/app/api/order/get-all-order/route.js
@@ -2,27 +2,25 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/DBConfig/connectDB.js";
 import Order from "@/models/order.model";
 
-
 export const GET = async (req) => {
-    connectDB()
+    connectDB();
     try {
-        const order = await Order.aggregate([
-            { $project: {_id: 0} }
-        ]);
+        // Fetch orders where isDone is either true or false
+        const order = await Order.find({ isDone: { $in: [true, false] } });
 
         if (!order || order.length === 0) {
             return NextResponse.json(
                 { message: "No order found" }, 
-                { status: 404 });
+                { status: 404 }
+            );
         }
 
         return NextResponse.json(order);
-        
+
     } catch (error) {
         return NextResponse.json(
             { error: "Something went wrong while fetching order" }, 
-            { status: 500 });
+            { status: 500 }
+        );
     }
 };
-
-
