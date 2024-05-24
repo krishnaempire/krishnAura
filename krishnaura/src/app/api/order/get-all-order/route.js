e55cbdb209bc49f0d2e4febd51faf6fb0be71888
krishnaempire <krishnaempire77@gmail.com>
2024-05-24 13:12:55 +0000

    static to dynamic API error fix test 1

--- krishnaura/src/app/api/order/get-all-order/route.js
+++ krishnaura/src/app/api/order/get-all-order/route.js
@@ -1,11 +1,10 @@
-import asyncHandler from "express-async-handler";
 import { NextResponse } from "next/server";
 import { connectDB } from "@/DBConfig/connectDB.js";
 import Order from "@/models/order.model";
 
-connectDB()
 
-export const GET = asyncHandler(async (req) => {
+export const GET = async (req) => {
+    connectDB();
     try {
         const order = await Order.aggregate([
             { $match: {} }
@@ -13,17 +12,35 @@ export const GET = asyncHandler(async (req) => {
 
         if (!order || order.length === 0) {
             return NextResponse.json(
-                { message: "No order found" }, 
-                { status: 404 });
+                { message: "No order found" },
+                {
+                    status: 404,
+                    headers: {
+                        'Cache-Control': 'no-store, max-age=0'
+                    }
+                }
+            );
         }
 
-        return NextResponse.json(order);
+        return NextResponse.json(
+            order,
+            {
+                status: 200,
+                headers: {
+                    'Cache-Control': 'no-store, max-age=0'
+                }
+            }
+        );
         
     } catch (error) {
         return NextResponse.json(
-            { error: "Something went wrong while fetching order" }, 
-            { status: 500 });
+            { error: "Something went wrong while fetching order" },
+            {
+                status: 500,
+                headers: {
+                    'Cache-Control': 'no-store, max-age=0'
+                }
+            }
+        );
     }
-});
-
-
+};
